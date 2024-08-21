3fc49af359ee51b727c4bbddc23c621612535121
Chintan16pr <prajapatichintan2005@gmail.com>
2024-08-21 15:32:23 +0530

    update category by chintan

---
+++ admin/update-category.php
@@ -0,0 +1,72 @@
+<?php include('partials/menu.php');?>
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Update Category</h1>
+
+        <br><br>
+
+        <?php 
+            // Check Wether the id is set or not
+            if(isset($_GET['id']))
+            {
+                // Get the id and all other details
+            }
+            else
+            {
+                // Redirect to Manage Category
+                header('location:'.SITEURL.'admin/manage-category.php');
+            }
+
+        ?>
+
+        <form action="" method="POST" enctype="multipart/form-data">
+            <table class="tbl-30">
+                <tr>
+                    <td>Title :</td>
+                    <td>
+                        <input type="text" name="title" value="">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Current Image: </td>
+                    <td>
+                        Image Will Be Displayed here
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>New Image: </td>
+                    <td>
+                        <input type="file" name="image">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Featured: </td>
+                    <td>
+                        <input type="radio" name="featured" value="Yes">Yes
+                        <input type="radio" name="featured" value="No">No
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td>Active: </td>
+                    <td>
+                        <input type="radio" name="active" value="Yes">Yes
+                        <input type="radio" name="active" value="No">No
+                    </td>
+                </tr>
+
+                <tr>
+                    <input type="submit" name="submit" value="update-category" class="btn-secondary"> 
+                </tr>
+
+            </table>
+        </form>
+
+    </div>
+</div>
+
+<?php include('partials/footer.php');?>
