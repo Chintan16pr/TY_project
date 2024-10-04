b49dbb3118217d746df9eac133c5bb57b7dc6f56
Chintan16pr <prajapatichintan2005@gmail.com>
2024-10-04 14:05:33 +0530

    order

--- order.php
+++ order.php
@@ -1,5 +1,42 @@
 <?php include("partials-front/menu.php"); ?>
 
+    <?php
+        // Check wether the food id is set or not
+        if(isset($_GET['food_id']))
+        {
+            // get the food id and details of selected food
+            $food_id = $_GET['food_id']; 
+
+            // get the details of the selected food
+            $sql = "SELECT * FROM tbl_food WHERE id = '$food_id'";
+            // Executing the query
+            $res = mysqli_query($conn, $sql);
+            // Count the number of rows returned
+            $count = mysqli_num_rows($res);
+
+            // Check Wether the data is available or not
+            if($count==1)
+            {
+                // we have data
+                $row = mysqli_fetch_assoc($res);
+                $title = $row['title'];
+                $price = $row['price'];
+                $image_name = $row['image_name'];
+            }
+            else
+            {
+                // we don't have data
+                header('location:'.SITEURL);
+            }  
+        }
+        else
+        {
+            // redirect to home page
+            header ('location:'.SITEURL);
+
+        }
+    ?>
+
     <!-- fOOD sEARCH Section Starts Here -->
     <section class="food-search">
         <div class="container">
@@ -11,12 +48,27 @@
                     <legend>Selected Food</legend>
 
                     <div class="food-menu-img">
-                        <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                        <?php
+                            // Check Wether the image is availabale or not
+                            if($image_name == "")
+                            {
+                                // image is not available
+                                echo "<div class='error'>Image not available.</div>";
+                            }
+                            else
+                            {
+                                // image is available
+                                ?>
+                                    <img src="<?php echo SITEURL;?>images/food/<?php echo $image_name;?>" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                                <?php
+                            }
+
+                        ?>
                     </div>
     
                     <div class="food-menu-desc">
-                        <h3>Food Title</h3>
-                        <p class="food-price">$2.3</p>
+                        <h3><?php echo $title; ?></h3>
+                        <p class="food-price">₹<?php echo $price; ?></p>
 
                         <div class="order-label">Quantity</div>
                         <input type="number" name="qty" class="input-responsive" value="1" required>
